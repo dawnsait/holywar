727221931684c47b470ff29d77bbb0940a78e1b0
dawnsait <giftdemon@gmail.com>
2011-12-12 10:26:47 +0800

    代码调整 Signed-off-by: dawnsait <giftdemon@gmail.com>

--- map/src/main/java/com/fasheng/map/utils/HttpCilentUtils.java
+++ map/src/main/java/com/fasheng/map/utils/HttpCilentUtils.java
@@ -10,9 +10,7 @@ import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
-import org.apache.http.client.ResponseHandler;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.BasicResponseHandler;
 import org.apache.http.impl.client.DefaultHttpClient;
 
 public class HttpCilentUtils {
