5d71259c5a277a18258a86582f50144f167f53d5
dawnsait <giftdemon@gmail.com>
2011-12-19 16:36:35 +0800

    日志框架优化,见PatternLayout
    Signed-off-by: dawnsait <giftdemon@gmail.com>

--- web/src/test/resources/db/database-test-data.sql
+++
@@ -1,6 +0,0 @@
--- ----------------------------
--- Records of user
--- ----------------------------
-INSERT INTO `user` VALUES ('1', 'C.Ronaldo', 'Portugal', '2011-10-30');
-INSERT INTO `user` VALUES ('2', 'messi', 'Argentina', '2011-10-30');
-INSERT INTO `user` VALUES ('3', 'kaka', 'Brazil', '2011-10-30');
