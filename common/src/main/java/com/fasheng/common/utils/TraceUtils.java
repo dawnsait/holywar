3db1b94fc6f862ccd1780c806d64996b42eeee6d
dawnsait <giftdemon@gmail.com>
2012-01-09 15:48:56 +0800

    跟踪外部调用情况时使用~
    Signed-off-by: dawnsait <giftdemon@gmail.com>

---
+++ common/src/main/java/com/fasheng/common/utils/TraceUtils.java
@@ -0,0 +1,35 @@
+package com.fasheng.common.utils;
+
+import org.apache.commons.lang.RandomStringUtils;
+import org.slf4j.MDC;
+
+/**
+ * @author vongosling.fengj 2012-1-9 03:17:09
+ */
+public class TraceUtils {
+
+    public static final String TRACE_ID_KEY    = "traceId";
+    public static final int    TRACE_ID_LENGTH = 8;
+
+    /**
+     * 开始Trace,生成本次Trace的ID,并放入MDC.
+     */
+    public static void beginTrace() {
+        String traceId = RandomStringUtils.randomAlphanumeric(TRACE_ID_LENGTH);
+        MDC.put(TRACE_ID_KEY, traceId);
+    }
+
+    /**
+     * 开始Trace, 将traceId放入MDC.
+     */
+    public static void beginTrace(String traceId) {
+        MDC.put(TRACE_ID_KEY, traceId);
+    }
+
+    /**
+     * 结束一次Trace. 清除traceId.
+     */
+    public static void endTrace() {
+        MDC.remove(TRACE_ID_KEY);
+    }
+}
